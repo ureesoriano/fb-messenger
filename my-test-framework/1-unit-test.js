93dcdd09430217ab9541ed9c9263462c320c4a38
Oriol Soriano <oriol.soriano@capside.com>
2018-06-06 17:50:34 +0000

    Add 'unit tests' test functions

--- my-test-framework/1-unit-test.js
+++ my-test-framework/1-unit-test.js
@@ -3,3 +3,25 @@ const sum = (a, b) => a + b
 const substract = (a, b) => a - b
 
 // Let's write some tests for those functions
+const testSum = () => {
+  const result = sum(1, 2);
+  const expected = 3;
+
+  if (result !== expected) {
+    throw new Error('KO - sum');
+  }
+  console.log('OK!');
+}
+
+const testSubstract = () => {
+  const result = substract(3, 2);
+  const expected = 1;
+
+  if (result !== expected) {
+    throw new Error('KO - subtract');
+  }
+  console.log('OK!');
+}
+
+testSum();
+testSubstract();
